18f638f4322269cf610f23d188eb0aec095de33d
Sanjil <sanzil99@gmail.com>
2023-10-31 10:46:48 -0400

    update the priority using useFetcher hook

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./ui/Home";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Order, { loader as orderLoader } from "./features/order/Order";
+import { action as updateOrderAction } from "./features/order/UpdateOrder";
 import CreateOrder, {
   action as createOrderAction,
 } from "./features/order/CreateOrder";
@@ -32,6 +33,7 @@ const router = createBrowserRouter([
         element: <Order />,
         loader: orderLoader,
         errorElement: <Error />,
+        action: updateOrderAction,
       },
       { path: "/cart", element: <Cart /> },
     ],
