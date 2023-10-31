21c7fbe75f08e3017caba1f808ff086b192a4c1e
Sanjil <sanzil99@gmail.com>
2023-10-31 10:24:18 -0400

    useFetcher hook implemented

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -1,6 +1,6 @@
 // Test ID: IIDSAT
 
-import { useLoaderData } from "react-router-dom";
+import { useFetcher, useLoaderData } from "react-router-dom";
 import { getOrder } from "../../services/apiRestaurant";
 import {
   calcMinutesLeft,
@@ -8,9 +8,19 @@ import {
   formatDate,
 } from "../../utils/helpers";
 import OrderItem from "./OrderItem";
+import { useEffect } from "react";
 
 function Order() {
   const order = useLoaderData();
+
+  const fetcher = useFetcher();
+
+  useEffect(
+    function () {
+      if (!fetcher.data && fetcher.state === "idle") fetcher.load("/menu");
+    },
+    [fetcher]
+  );
 
   const {
     id,
@@ -53,7 +63,14 @@ function Order() {
 
       <ul className="divide-y divide-stone-200 border-b border-t">
         {cart.map((item) => (
-          <OrderItem item={item} key={item.pizzaId} />
+          <OrderItem
+            item={item}
+            key={item.pizzaId}
+            isLoadingIngredients={fetcher.state === "loading"}
+            ingredients={
+              fetcher?.data?.find((el) => el.id === item.pizzaId)?.ingredients
+            }
+          />
         ))}
       </ul>
 
--- src/features/order/OrderItem.jsx
+++ src/features/order/OrderItem.jsx
@@ -5,13 +5,16 @@ function OrderItem({ item, isLoadingIngredients, ingredients }) {
   const { quantity, name, totalPrice } = item;
 
   return (
-    <li className="py-3">
+    <li className="py-3 space-y-1">
       <div className="flex items-center justify-between gap-4 text-sm">
         <p>
           <span className="font-bold">{quantity}&times;</span> {name}
         </p>
         <p className="font-bold">{formatCurrency(totalPrice)}</p>
       </div>
+      <p className="text-sm capitalize italic">
+        {isLoadingIngredients ? "Loading ..." : ingredients?.join(", ")}
+      </p>
     </li>
   );
 }
@@ -19,7 +22,7 @@ function OrderItem({ item, isLoadingIngredients, ingredients }) {
 OrderItem.propTypes = {
   item: PropTypes.object,
   isLoadingIngredients: PropTypes.bool,
-  ingredients: PropTypes.object,
+  ingredients: PropTypes.array,
 };
 
 export default OrderItem;
